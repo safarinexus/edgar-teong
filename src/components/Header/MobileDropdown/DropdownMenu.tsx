a32546094595769944766cf849fe596304c862d0
Edgar <edgargarteong@gmail.com>
2024-10-13 04:40:36 +0800

    fixed reactportal error, fixed commits

--- src/components/Header/MobileDropdown/DropdownMenu.tsx
+++ src/components/Header/MobileDropdown/DropdownMenu.tsx
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom"
 
-import ReactDOM from "react-dom"
-
 interface Props {
     open: boolean;
     setOpen: (open: boolean) => void;
@@ -11,18 +9,18 @@ const DropdownMenu = ({ open, setOpen }: Props) => {
     let nameClass = ""; 
     if (open) { 
         nameClass = "bg-beige dark:bg-black origin-top\
-                flex flex-col self-end transition-transform\
+                flex flex-col transition-transform right-0\
                 rounded-3xl w-32 h-40 pr-7 pl-7 pt-4 pb-4\
                 justify-around scale-100 absolute -bottom-40"
     } else {
         nameClass = "bg-beige dark:bg-black origin-top\
-                flex flex-col self-end transition-transform\
+                flex flex-col transition-transform right-0\
                 rounded-3xl w-32 h-40 pr-7 pl-7 pt-4 pb-4\
                 justify-around scale-0 absolute -bottom-40"
     }
 
 
-    return ReactDOM.createPortal(
+    return (
         <div
             id="dropdown-menu"
             className={nameClass}
@@ -31,8 +29,7 @@ const DropdownMenu = ({ open, setOpen }: Props) => {
             <Link to="#projects" onClick={() => setOpen(false)}>Projects</Link>
             <Link to='#contact' onClick={() => setOpen(false)}>Contact</Link>
             <Link to='/blog' onClick={() => setOpen(false)}>Blog</Link>
-        </div>, 
-        document.getElementById('header-container') as HTMLElement
+        </div>
     )
 }
 
