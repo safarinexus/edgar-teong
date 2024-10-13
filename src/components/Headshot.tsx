6265ffabfc7faf4c9bd3619ce9e1a7dc6a05069e
Edgar <edgargarteong@gmail.com>
2024-10-14 01:36:14 +0800

    little detail

--- src/components/Headshot.tsx
+++ src/components/Headshot.tsx
@@ -7,7 +7,7 @@ const Headshot = () => {
         <div className="absolute size-56 bg-gradient-to-br from-gray-500 to-rose-700 rounded-full animate-breathing"></div>
         
         {/* Inner circle with profile image */}
-        <div className="relative size-52 border-white border-2 bg-white rounded-full overflow-hidden">
+        <div className="relative size-52 border-black dark:border-white border-2 bg-white rounded-full overflow-hidden">
           <img
             src={HeadshotPhoto} 
             alt="Headshot Photo"
