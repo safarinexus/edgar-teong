9cedb07337e7de47138e26d081d349ce506cbf53
Edgar <edgargarteong@gmail.com>
2024-10-14 09:41:40 +0800

    placeholder

--- src/components/PageComponents/Landing.tsx
+++ src/components/PageComponents/Landing.tsx
@@ -6,7 +6,7 @@ const Landing = () => {
     return (
     <div id="landing" className="h-[1000px] pt-20 flex flex-col items-center">
         <Headshot />
-        <h1 className="mt-8 font-inter text-8xl font-bold text-center">Hey there! 👋</h1>
+        <h1 className="mt-8 font-inter text-8xl font-bold text-center">👋 Hey there!</h1>
         <h3 className="mt-8 font-domine text-3xl text-center">My name is <span className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">Edgar Teong</span>.</h3>
       </div>
     )
