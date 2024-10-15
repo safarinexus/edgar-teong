ef64149de32e584af63f2cb3bfa8c9421bdaf6d1
Edgar <edgargarteong@gmail.com>
2024-10-15 13:40:49 +0800

    an attempt at fixing mobile viewport height

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,43 @@ import Header from "./components/Header/Header"
 import ScrollProgressBar from "./components/ScrollProgressBar"
 import Landing from "./components/PageComponents/Landing/Landing"
 
+interface Dimension {
+  height: number; 
+  width: number;
+}
+
 const App = () => {
-  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [dimensions, setDimensions] = useState<Dimension>({
+    height: window.innerHeight, 
+    width: window.innerWidth, 
+  });
+
+  const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
 
   useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    function handleResize() {
+      setDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
+
+  useEffect(() => {
+    console.log(dimensions.height);
+  }, [dimensions])
 
   return (
     <>
       <ScrollProgressBar />
-      <Header width={width}/>
-      <Landing />
+      <Header width={dimensions.width}/>
+      <Landing height={dimensions.height}/>
       <div id="work" className="h-svh">I'm working late, cuz I'm a singer</div>
       <div id="projects" className="h-svh">projects here</div>
       <div id="contact" className="h-svh">Contact Me</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ export default {
         wave: '1s wave 1.5s ease-in-out 1',
         updown: '1s updown 1.5s ease-out infinite',
         fadeIn: '0.5s fadeIn 1s ease-in forwards',
+        textFadeIn: 'textFadeIn 1s ease-out forwards', 
       },
       keyframes: {
         wave: {
@@ -34,6 +35,10 @@ export default {
         fadeIn: {
           '0%': { opacity: '0' }, 
           '100%': { opacity: '1' },
+        },
+        textFadeIn: {
+          '0%': { transform: 'translateY(-20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
         },
       },
     },
