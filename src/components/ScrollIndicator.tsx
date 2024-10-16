90635be72754cfd0034d78fb633c578b8a889331
Edgar <edgargarteong@gmail.com>
2024-10-17 01:00:56 +0800

    slight tweak to scroll indicator

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -29,12 +29,12 @@ const ScrollIndicator = ({ height }: Props) => {
     <div id="scroll-indicator-container" className="fixed w-full flex z-10 flex-col items-center space-y-1 bottom-5 will-change-scroll" style={{ opacity }}>
       
         { height < 725 ? ("") : 
-          <HashLink to="/#work" id="scroll-text" className="opacity-0 animate-fadeIn bg-beige dark:bg-black p-1 rounded-md">
+          <HashLink to="/#about" id="scroll-text" className="opacity-0 animate-fadeIn bg-beige dark:bg-black p-1 rounded-md">
            <p className="text-xs">Scroll to Discover</p>
           </HashLink>
         }
       
-      <HashLink to="/#work" id="scroll-indicator" className="relative flex items-center justify-center w-7 h-7 rounded-full bg-amber-400">
+      <HashLink to="/#about" id="scroll-indicator" className="relative flex items-center justify-center w-7 h-7 rounded-full bg-amber-400">
         <FaArrowDown className="animate-indicateScroll"/>
       </HashLink>
     </div>
