e0e1957c064f43617d0184347fe64432b6e14314
Edgar <edgargarteong@gmail.com>
2024-10-17 01:27:46 +0800

    tweak to landing page layout

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -4,26 +4,25 @@ const Landing = () => {
     return (
         <div 
             id="landing" 
-            className=" w-full h-full flex flex-col
+            className=" w-full h-full flex flex-col font-light text-base
                 sm:grid sm:gap-4 sm:items-center sm:grid-cols-[60%_40%] sm:grid-rows-[10%_auto_20%]
             "
         >
             <Headshot />
             
-            <h1 id="hey-there"className="font-light text-base mt-4 sm:mt-0 sm:col-start-1 sm:row-start-1 sm:self-end">
+            <h1 id="hey-there"className="mt-4 sm:mt-0 sm:col-start-1 sm:row-start-1 sm:self-end">
                 <span className="animate-wave inline-block">👋</span> 
                 &nbsp;Hey there!
             </h1>
 
-            <h3 id="my-name-is" className="h-max font-light text-[22px] leading-none sm:col-start-1 sm:row-start-3 sm:col-span-2 mt-4 sm:mt-0">
-                My name is&nbsp;
-                <span className="relative h-[60px] text-5xl bg-gradient-to-r from-brown via-green-600 to-sky-500 inline-block text-transparent bg-clip-text">
+            <h3 id="my-name-is" className="h-max leading-none sm:col-start-1 sm:row-start-3 sm:col-span-2 mt-4 sm:mt-0">
+                My name is<br/>
+                <span className="inline-block relative font-extralight h-[60px] text-5xl bg-gradient-to-r from-brown via-green-600 to-sky-500 text-transparent bg-clip-text">
                     Edgar Teong
                     <svg className="absolute -bottom-2 w-full scale-0 origin-left transition-transfor ease-out animate-highlight left-0 h-auto -z-10 transform translate-y-1/3 saturate-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 20">
                         <path d="M0,5 C50,7 150,7 200,5" stroke="#F2CD02" fill="none" stroke-width="9" stroke-linecap="round"/>
                     </svg>
                 </span>
-                .
             </h3>
 
             <p className="tracking-wide leading-[19px] sm:text-left font-light text-sm xs:text-base md:text-base lg:text-xl sm:row-start-2 animate-textFadeIn mt-6 sm:mt-0">
