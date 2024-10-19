61dcffd7b8ca08bfc2f7f3246c0e9f7938486f38
Edgar <edgargarteong@gmail.com>
2024-10-20 04:30:23 +0800

    added some image preload

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,12 +5,12 @@ import { useGSAP } from '@gsap/react';
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
-import image1 from "../../assets/image1.webp"
-import image2 from "../../assets/image2.webp"
-import image3 from "../../assets/image3.webp"
-import image4 from "../../assets/image4.webp"
-import image5 from "../../assets/image5.webp"
-import image6 from "../../assets/image6.webp"
+import image1 from "../../../public/image1.webp"
+import image2 from "../../../public/image2.webp"
+import image3 from "../../../public/image3.webp"
+import image4 from "../../../public/image4.webp"
+import image5 from "../../../public/image5.webp"
+import image6 from "../../../public/image6.webp"
 
 const About = () => {
     useGSAP(() => {
@@ -73,8 +73,7 @@ const About = () => {
                 <div id="image1-container" className="relative z-30 w-[130%] -rotate-12 bottom-[-15%] left-[5%] sm:bottom-0">
                     <img 
                         src={image1}
-                        id="image1" 
-                        loading="lazy" 
+                        id="image1"  
                         alt="image 1"
                         className="select-none rounded-xl brightness-90"
                         draggable="false"
@@ -83,8 +82,7 @@ const About = () => {
                 <div id="image2-container" className="relative z-20 rotate-6 -bottom-[6%] right-[-6%] sm:bottom-0 sm:right-[-15%]">
                     <img 
                         src={image2}
-                        id="image2" 
-                        loading="lazy" 
+                        id="image2"  
                         alt="image 2"
                         className="select-none rounded-xl brightness-90"
                         draggable="false"
@@ -93,8 +91,7 @@ const About = () => {
                 <div id="image3-container" className="relative z-10 w-[105%] rotate-12 bottom-[15%] left-[-5%] sm:w-[95%] sm:left-[10%] sm:bottom-0">
                     <img 
                         src={image3}
-                        id="image3" 
-                        loading="lazy" 
+                        id="image3"  
                         alt="image 3"
                         draggable="false"
                         className="select-none rounded-xl brightness-90"
@@ -103,8 +100,7 @@ const About = () => {
                 <div id="image4-container" className="relative z-20 hidden sm:block w-[110%] rotate-12 left-[5%] bottom-[32%]">
                     <img 
                         src={image4}
-                        id="image4" 
-                        loading="lazy" 
+                        id="image4"  
                         alt="image 4"
                         className="select-none rounded-xl brightness-90"
                         draggable="false"
@@ -113,8 +109,7 @@ const About = () => {
                 <div id="image5-container" className="relative -rotate-6 w-[130%] top-[-18%] sm:w-[140%] sm:top-[-25%]">
                     <img 
                         src={image5}
-                        id="image5" 
-                        loading="lazy" 
+                        id="image5"  
                         alt="image 1"
                         className="select-none rounded-xl brightness-90"
                         draggable="false"
@@ -123,8 +118,7 @@ const About = () => {
                 <div id="image6-container" className="relative -z-10 hidden sm:block w-[95%] -rotate-12 left-[10%] top-[-26%]">
                     <img 
                         src={image6}
-                        id="image6" 
-                        loading="lazy" 
+                        id="image6"  
                         alt="image 1"
                         className="select-none rounded-lg"
                         draggable="false"
