da302d556329a18671e8a365d293b1406e9d469e
Edgar <edgargarteong@gmail.com>
2024-10-21 06:36:17 +0800

    changed mobile animations

--- src/components/Work&Projects/Work&Projects.tsx
+++ src/components/Work&Projects/Work&Projects.tsx
@@ -5,16 +5,36 @@ import { useGSAP } from '@gsap/react';
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
+import works from "../../data/works"
+
 const WorkProjects = () => {
-    
+    useGSAP(() => {
+        const mm = gsap.matchMedia();
+        mm.add("(prefers-reduced-motion: no-preference)", () => {
+            ScrollTrigger.create({
+                trigger: "#work-text", 
+                start: "center center", 
+                scrub: true,
+                pinSpacing: false,
+                pin: "#work-text", 
+            })
+        })
+    });
 
     return (
         <>
-            <div id="work" className="h-[1000px] w-full">
-
+            <div id="work" className="w-full min-h-svh flex pt-20">
+                <h1 id="work-text" className="font-bold text-4xl w-[50svw] h-[250px] pl-24">My Work<br />Experience</h1>
+                <div className="w-[50svw] max-w-[770px] pr-24">
+                    <div className="h-[250px] w-full grid gap-1 grid-cols-[30%_70%] grid-rows-[10%_70%_20%] border-white border-2">
+                        <p>Jun. 2022 - Aug. 2022</p>
+                        <p>Accenture | Software Development Engineer in Test Intern</p>
+                        <p>Wrote tests and stuff</p>
+                        <div></div>
+                    </div>
+                </div>
             </div>
-            <div id="projects" className="h-[1000px] w-full">
-                shut the front door
+            <div id="projects" className="h-[1000px] w-full border-white border-2">
             </div>
         </>
     )
---
+++ src/data/works.ts
@@ -0,0 +1,19 @@
+interface Work {
+    dates: string, 
+    company: string, 
+    position: string, 
+    description: string, 
+    technologies: string[],
+}
+
+const works: Work[] = [
+    {
+        dates: "June 2022 - August 2022", 
+        company: "Accenture", 
+        position: "Software Development Engineer in Test Intern", 
+        description: "yes", 
+        technologies: ["Python", "SOAP APIs"],
+    },
+]
+
+export default works
