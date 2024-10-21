64766b91fbc5fbfe915e68878111693aa690b142
Edgar <edgargarteong@gmail.com>
2024-10-21 23:42:50 +0800

    undo

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,7 +1,7 @@
 const Error = () => {
     return (
         <div id="error-container" className="w-full h-svh flex flex-col justify-center items-center">
-            <h1>Oops! Page not found!</h1>
+            <h1 className="font-medium text-3xl">Oops! Page not found!</h1>
         </div>
     )
 }
