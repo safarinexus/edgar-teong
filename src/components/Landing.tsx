d7eeb42beb435b9d0ea15aab3a426e4e509a268c
Edgar <edgargarteong@gmail.com>
2024-10-22 03:34:35 +0800

    commit to undo

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,14 +2,10 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from '@gsap/react';
 
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaCodepen } from "react-icons/fa";
-import { FaGoodreads } from "react-icons/fa";
-import { BiLogoGmail } from "react-icons/bi";
-
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
+
+import ContactBar from "./ContactBar"
 
 const Landing = () => {
     useGSAP (() => {
@@ -92,23 +88,7 @@ const Landing = () => {
                     </span>
                 </div>
             </div>
-            <div className="h-10 w-56 mt-5 mx-auto sm:m-0 flex justify-between items-center z-30"> 
-                <a href="mailto:edgargarteong@gmail.com" target="_blank">
-                    <BiLogoGmail className="size-8 sm:size-6"/>
-                </a>
-                <a href="https://github.com/safarinexus">
-                    <FaGithub className="size-7 sm:size-6"/>
-                </a>
-                <a href="https://codepen.io/safarinexus">
-                    <FaCodepen className="size-7 sm:size-6"/>
-                </a>
-                <a href="https://linkedin.com/in/edgarteong">
-                    <FaLinkedin className="size-8 sm:size-6"/>
-                </a>
-                <a href="https://goodreads.com/user/show/80321969-edgar">
-                    <FaGoodreads className="size-8 sm:size-6"/>
-                </a>
-            </div>
+            <ContactBar />
         </div>
     )
 }
