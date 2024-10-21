d22a9036f431cae6131cf1fbd6808a0d19e3cb97
Edgar <edgargarteong@gmail.com>
2024-10-21 22:12:08 +0800

    overhauled landing

--- src/components/Header/DarkMode.tsx
+++ src/components/Header/DarkMode.tsx
@@ -34,7 +34,7 @@ export default function DarkMode() {
             className="size-6 hover:opacity-40 duration-500 transition-opacity motion-reduce:transition-none text-black dark:text-white z-10"
             onClick={handleClick}
         >
-            {darkMode === "true" ? (<MdLightMode className="size-6 sm:size-7"/>) : (<MdDarkMode className="size-5 sm:size-7"/>)}
+            {darkMode === "true" ? (<MdLightMode className="size-6"/>) : (<MdDarkMode className="size-5"/>)}
         </button>
     )
 }
