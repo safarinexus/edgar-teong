01007306eb2e34d01a7bf0e787fd3986867cbd30
Edgar <edgargarteong@gmail.com>
2024-10-22 15:04:16 +0800

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,14 @@ const Footer = () => {
     return (
         <div id="footer" className="mt-10 w-full space-y-2">
             <hr className="w-[80%] mx-auto"/>
-            <a 
-                href="https://github.com/safarinexus/edgar-teong"
-                className="block font-light text-sm w-full text-neutral-500 text-center"
-            >
-                Made by Edgar Russell Teong Kai Han
-            </a>
+            <div className="w-full flex justify-center">
+                <a 
+                    href="https://github.com/safarinexus/edgar-teong"
+                    className="font-light text-sm w-fit leading-[1.2] z-10"
+                >
+                    Made by Edgar Russell Teong Kai Han
+                </a>
+            </div>
             <div className="w-full flex justify-center">
                 <ContactBar />
             </div>
