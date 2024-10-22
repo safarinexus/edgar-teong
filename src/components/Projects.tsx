ffa87c304ce10140ee101aad744ddcfad37a3743
Edgar <edgargarteong@gmail.com>
2024-10-22 16:13:20 +0800

    safari sux

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -28,7 +28,7 @@ const Projects = () => {
                 return (
                     <div key={index} id={"project"+index} className={"font-medium w-full max-w-[600px] h-auto rounded-xl p-3 bg-white\
                     border-[1px] border-neutral-300 dark:border-neutral-800 shadow-custom dark:shadow-custom-dark dark:bg-black\
-                    grid gap-2 grid-cols-1 grids-rows-[50%_10%_15%_25%] my-9 self-center" + skew}
+                    inline-grid gap-2 grid-cols-1 grids-rows-[50%_10%_15%_25%] my-9 self-center" + skew}
                     >
                         <img 
                             src={project.image}
