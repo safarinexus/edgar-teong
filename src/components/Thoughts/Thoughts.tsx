ad8977fe1c8067414160515b3e50469ddc9a7f20
Edgar <edgargarteong@gmail.com>
2024-10-23 15:47:43 +0800

    updated thoughts page

--- src/components/Thoughts/Thoughts.tsx
+++ src/components/Thoughts/Thoughts.tsx
@@ -1,10 +1,14 @@
-const thoughts = () => {
+const Thoughts = () => {
     return (
-        <div className="w-full h-svh flex flex-col justify-center items-center">
-            <h1 className="font-medium text-3xl">Oops! Page not found!</h1>
-            <p className="font-light text-base">Click my logo to head back 😉</p>
+        <div id="thoughts-container" className="w-full h-svh px-2 pt-2">
+            <div className="w-full max-w-[1192px] h-[400px] p-10 mx-auto flex flex-col justify-end rounded-[32px] bg-white border-[1px] 
+            border-neutral-300 dark:border-neutral-800 shadow-custom dark:shadow-custom-dark dark:bg-black"
+            >
+                <h1 className="font-medium text-5xl">Coming Soon...</h1>
+                <p className="font-light text-base">Click my logo to head back 😉</p>
+            </div>
         </div>
     )
 }
 
-export default thoughts
+export default Thoughts
