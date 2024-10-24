12df25bef5d72fae33304b6a348938230739a5f3
Edgar <edgargarteong@gmail.com>
2024-10-24 18:57:55 +0800

    refactored scroll indicator to use gsap

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { gsap } from "gsap"; 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from '@gsap/react';
@@ -14,63 +13,52 @@ interface Props {
 }
 
 const ScrollIndicator = ({ height }: Props) => {
-  const [opacity, setOpacity] = useState<number>(1);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      const maxScroll = 300;
-      const newOpacity = Math.max(1 - scrollTop / maxScroll, 0);
-      setOpacity(newOpacity);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   useGSAP (() => {
     const mm = gsap.matchMedia();
     mm.add("(prefers-reduced-motion: no-preference)", () => {
-      gsap.from('#scroll-indicator-container', { 
-          duration: 0.5,
-          delay: 0.5, 
-          ease: "power3.out",
-          y: "-20%", 
-          opacity: 0,
-          onComplete: () => {
-            gsap.to("#scroll-arrow", {
-              y: 3, 
-              duration: 0.25, 
-              ease: "power1.inOut",
-              onComplete: () => {
-                gsap.fromTo('#scroll-arrow', {
-                  y: 3, 
-                  duration: 0.25,
-                }, {
-                  y: -3, 
-                  duration: 0.5, 
-                  repeat: -1, 
-                  yoyo: true,
-                  ease: "power1.inOut",
-              })
-              }
-            })
-          }
-        });
+      const tl = gsap.timeline(); 
+      tl.to("#scroll-indicator-container", {
+        opacity: 0,
+        scale: 0.9,
+        scrollTrigger: {
+          trigger: "#scroll-indicator-container", 
+          start: "clamp(90% bottom)", 
+          end: "clamp(bottom 75%)",
+          scrub: 1,
+        }
+      }).from('#scroll-indicator-container', { 
+        duration: 0.5,
+        delay: 0.5, 
+        ease: "power3.out",
+        y: "-20%", 
+        opacity: 0,
+      }).fromTo("#scroll-arrow", {
+        y: 0, 
+        duration: 0.25, 
+      }, {
+        y: 3,
+        ease: "power1.inOut"
+      }).fromTo('#scroll-arrow', { 
+        duration: 0.25,
+      }, {
+        y: -3, 
+        duration: 0.5, 
+        repeat: -1, 
+        yoyo: true,
+        ease: "power1.inOut",
+      })
     });
   });
 
   return (
-    <div id="scroll-indicator-container" className="fixed w-full flex z-10 flex-col items-center space-y-1 bottom-5 will-change-scroll" style={{ opacity }}>
+    <div id="scroll-indicator-container" className="fixed w-full flex z-10 flex-col items-center space-y-1 bottom-5 will-change-scroll">
       
-        { height < 725 ? ("") : 
-          <HashLink to="/#about" id="scroll-text" className="bg-beige dark:bg-black p-1 rounded-md">
-           <p className="text-xs">Scroll to Discover</p>
-          </HashLink>
-        }
+      { height < 720 ? ("") : 
+        <HashLink to="/#about" id="scroll-text" className="p-1 rounded-md">
+          <p className="text-xs">Scroll to Discover</p>
+        </HashLink>
+      }
       
       <HashLink to="/#about" id="scroll-indicator" className="relative flex items-center justify-center w-7 h-7 rounded-full bg-amber-400">
         <FaArrowDown id="scroll-arrow"/>
