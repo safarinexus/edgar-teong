b62a5aaaf1fd01deca0edf7a678bcc155d289edc
Edgar <edgargarteong@gmail.com>
2024-10-27 03:16:44 +0800

    added headshot back to hero

---
+++ src/components/Headshot.tsx
@@ -0,0 +1,19 @@
+import HeadshotPhoto from "/headshot.webp"
+
+const Headshot = () => {
+
+  return (
+      <div id="headshot-container" className="relative flex justify-center items-center">
+        <div id="headshot" className="relative max-w-20 border-black dark:border-white border-[1px] bg-white rounded-full overflow-hidden">
+          <img
+            src={HeadshotPhoto} 
+            alt="Headshot Photo"
+            className="w-full h-full select-none"
+            draggable="false"
+          />
+        </div>
+      </div>
+  );
+};
+
+export default Headshot;
