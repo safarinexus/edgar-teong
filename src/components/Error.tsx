def059694c1f4df2368f2dafe0379cb2be58ffad
Edgar <edgargarteong@gmail.com>
2024-10-26 14:58:14 +0800

    changed codepen to ig; updated error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -4,8 +4,8 @@ const Error = () => {
             <div className="w-full max-w-[1192px] h-[400px] p-10 mx-auto flex flex-col justify-end rounded-[32px] bg-white border-[1px] 
             border-neutral-300 dark:border-neutral-800 shadow-custom dark:shadow-custom-dark dark:bg-black"
             >
-                <h1 className="font-medium text-3xl">Oops! Page not found!</h1>
-                <p className="font-light text-base">Click my logo to head back 😉</p>
+                <h1 className="font-medium text-5xl">404 | Page not found</h1>
+                <p className="font-light text-base mt-5">Click my logo to head back 😉</p>
             </div>
         </div>
     )
