9bbcd0750e3db66f212a7013596a164c519970fc
Edgar <edgargarteong@gmail.com>
2024-10-31 22:00:40 +0800

    slight footer tweak

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import ContactBar from "./ContactBar";
 const Footer = () => {
     return (
         <footer className="mt-10 w-full">
-            <hr className="w-[80%] mx-auto" style={{ color: "#737373", opacity: '50%' }}/>
+            <hr className="w-[80%] mx-auto max-w-[1176px]" style={{ color: "#737373", opacity: '50%' }}/>
             <div className="space-y-3 my-4">
                 <div className="w-full flex justify-center">
                     <a 
