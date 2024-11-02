ea319ff5324345286f9bd1d56c4f14ccc1eab616
Edgar <edgargarteong@gmail.com>
2024-11-02 13:45:15 +0800

    added speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -143,8 +143,8 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
+          <SpeedInsights />
         </Providers>
-        <SpeedInsights />
       </body>
     </html>
   );
