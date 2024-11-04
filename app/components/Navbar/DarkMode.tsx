00e5eb85d3e92689ed6daffb34103b09454dc8d1
Edgar <edgargarteong@gmail.com>
2024-11-04 19:56:51 +0800

    tweak for system darkmode

--- app/components/Navbar/DarkMode.tsx
+++ app/components/Navbar/DarkMode.tsx
@@ -17,7 +17,7 @@ export default function DarkMode() {
     if (theme === "system") { 
         return (
         <button type="button" aria-label="dark mode button system" className="relative group hover:opacity-100">
-            <div className="absolute -top-[10px] -left-[10.5px] bg-neutral-700 rounded-full w-11 h-11 opacity-0 sm:group-hover:opacity-65 sm:transition-opacity sm:duration-500"></div>
+            <div className="absolute -top-[10px] -left-[10.5px] bg-neutral-300 dark:bg-neutral-700 rounded-full w-11 h-11 opacity-0 sm:group-hover:opacity-65 sm:transition-opacity sm:duration-500"></div>
             <RiComputerLine className="relative size-6 z-10 cursor-pointer" onClick={()=> setTheme('dark')}/>
         </button>
         )
