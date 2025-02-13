f249ecd75d04711c5cb5170ece52b6d9277db062
Edgar <edgargarteong@gmail.com>
2025-02-14 01:21:18 +0800

    updated text

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -217,13 +217,15 @@ const About = () => {
             <div id="about-text-container" className="w-full px-5 sm:px-24 font-light text-base">
                 <h1 className="font-bold text-2xl sm:text-4xl">About Me</h1>
                 <p id="about-body" className="mt-7 tracking-tight leading-relaxed font-light text-base lg:text-xl">
-                I&#39;m currently a Full Stack Software Engineer at <a href="https://climateimpactx.com/"><span className="font-bold">Climate Impact X</span></a>. I graduated from  <a href="https://www.smu.edu.sg/"><span className="font-bold">Singapore Management University</span> with a Bachelor&#39;s in <span className="font-bold">Accountancy &amp; Information Systems</span></a>.
+                I&#39;m currently a Full Stack Developer at <a href="https://climateimpactx.com/"><span className="font-bold">Climate Impact X</span></a>. I graduated from  <a href="https://www.smu.edu.sg/"><span className="font-bold">Singapore Management University</span> with a Bachelor&#39;s in <span className="font-bold">Accountancy with a Second Major in Information Systems</span></a>.
+                I transitioned into Software Engineering halfway through my Bachelor&#39;s, all by myself. 
                 <br /><br />
                 I am deeply passionate and self-driven. I taught myself programming from scratch and full stack web development with <a href="https://www.theodinproject.com/about" className="font-bold">The Odin Project</a>. 
-                {" "}It is one of my goals in life to be able to use my love for computers and technology to make a real difference to society through software. 
+                I continue to learn computer science theory in my free time.
+                It is one of my goals in life to be able to use my love for computers and technology to make a real difference to society through software. 
                 <br /><br />
-                When I&#39;m not coding, you&#39;ll find me making filter/espresso coffee, hanging with friends, enjoying obscure films, tinkering with keyboards,
-                {" "}reading up about psychology/mental health stuff, or dancing at my <a href="https://www.instagram.com/smuemix/" className="font-bold">school&#39;s hip-hop dance club</a>.
+                When I&#39;m not coding, you&#39;ll find me making filter/espresso coffee ☕️, hanging with friends 🎉, enjoying obscure films 🍿, tinkering with mechanical keyboards ⌨️, 
+                reading up about psychology/mental health stuff 📚, or dancing at my <a href="https://www.instagram.com/smuemix/" className="font-bold">school&#39;s hip-hop dance club 🕺🏻</a>.
                 </p>
             </div>
         </>
