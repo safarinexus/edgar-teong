ba783a1962c2a399577c7a3655fb4b0674a0be84
Edgar Teong <edgargarteong@gmail.com>
2025-02-25 14:07:24 +0800

    revert 1

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -11,8 +11,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 import { works }from "@/lib/data";
 
-import TechIcon from "./TechIcon";
-
 import { MdArrowOutward } from "react-icons/md";
 
 const Work = () => {
@@ -37,48 +35,94 @@ const Work = () => {
     });
     */
 
-    const currJob = works[0]
     const smallWorks = works.slice(1);
 
     return (
         <>
             <h1 className="font-bold text-2xl sm:text-4xl">My Work Experience</h1>
             {
-            //curr job (big card on desktop)
+            //element for curr job (big card on desktop)
             }
-            <div className="mt-9 gap-9 grid md:grid-cols-2">
-                <div
-                    key={0} 
-                    id={'work'+0} 
-                    aria-label={currJob.company + " website"}
-                    className="block p-5 pb-16 font-medium bg-zinc-100
-                        rounded-[36px] text-black opacity-100 transition-opacity duration-75 lg:col-span-2 relative"
+            <div
+                key={0} 
+                id={'work'+0} 
+                aria-label={works[0].company + " website"}
+                className={`block group mt-9 sm:p-5 font-medium sm:grid sm:gap-2 grid-cols-[110px_auto] bg-${works[0].tailwindBgColor}
+                    rounded-3xl text-black opacity-100 transition-opacity duration-75`}
+            >
+                <p className="text-sm sm:text-lg sm:font-light sm:row-span-3 sm:my-[5px]">{works[0].dates}</p>
+                <a 
+                    className="text-lg font-bold tracking-tighter my-1 text-black sm:text-xl group"
+                    target="_blank"
+                    href={works[0].link} 
                 >
-                    <a 
-                        className="text-lg font-bold tracking-tighter my-1 text-black sm:text-xl group"
-                        target="_blank"
-                        href={currJob.link} 
-                    >
-                        <Image
-                                src={currJob.logo.url}
-                                height={currJob.logo.height}
-                                width={currJob.logo.width}
-                                className="inline w-6 sm:w-10" 
-                                alt={currJob.company + " logo"}
-                        />
-                        {currJob.company}
-                        <MdArrowOutward id="work-link-arrow" className="inline w-5 ml-1 group-hover:animate-hoverDiagonal transition-transform"/>
-                    </a>
-                    <p className="text-sm sm:font-light sm:my-[5px]">{currJob.dates}</p>
-                    <hr className='mt-2 mx-auto opacity-40 border-black' />  
-                    <h2 className='font-bold'>{currJob.position}</h2>
-                    { currJob.description !== undefined ? (<p className="my-3 font-light">{currJob.description}</p>) : null }
-                    { currJob.technologies !== undefined ?
-                        ( <div className="flex space-x-3 absolute bottom-5 mt-5 w-full">
-                            { currJob.technologies.map((tech, techIndex) => (
-                            <span key={techIndex}>
-                                    < TechIcon size={"size-5 lg:size-7"} technology={tech} />
+                    <Image
+                            src={works[0].logo.url}
+                            height={works[0].logo.height}
+                            width={works[0].logo.width}
+                            className="inline w-6 sm:w-10" 
+                            alt={works[0].company + " logo"}
+                    />
+                    {works[0].company} • {works[0].position}
+                    <MdArrowOutward id="work-link-arrow" className="inline w-5 ml-1 group-hover:animate-hoverDiagonal transition-transform"/>
+                </a>
+                { works[0].description !== undefined ? (<p className="my-3 font-light lg:text-xl">{works[0].description}</p>) : null }
+                { works[0].technologies !== undefined ?
+                    ( <div>
+                        { works[0].technologies.map((tech, techIndex) => (
+                            <span 
+                                key={techIndex} 
+                                className="inline-block px-3 py-1 mx-1 my-1 rounded-3xl bg-yellow-300 bg-opacity-80
+                                dark:bg-yellow-400 dark:bg-opacity-50 text-sm font-semibold text-black dark:text-white"
+                            >
+                                {tech}
                             </span>
+                        
+                        ))}
+                    </div> ) :
+                    null
+                }
+            </div>
+            {
+            //mapping elements for prev exp/internships (small cards on desktop)
+            }
+            {smallWorks.map((work, index) => {
+                const adjIndex = index + 1;
+                return (
+                <a 
+                    target="_blank"
+                    key={adjIndex} 
+                    id={"work"+adjIndex} 
+                    href={work.link} 
+                    aria-label={work.company + " website"}
+                    className={`block group mt-9 sm:p-5 font-medium sm:grid sm:gap-2 grid-cols-[110px_auto] bg-${work.tailwindBgColor ? work.tailwindBgColor : "white"}
+                    rounded-3xl text-black transition-opacity duration-75`}
+                >
+                    <p className="text-sm sm:text-lg sm:font-light sm:row-span-3 sm:my-[5px]">{work.dates}</p>
+                    <h1 className="text-lg font-bold tracking-tighter my-1 sm:text-xl">
+                        <Image
+                                src={work.logo.url}
+                                height={work.logo.height}
+                                width={work.logo.width}
+                                className="inline w-6 sm:w-10" 
+                                alt={work.company + " logo"}
+                        />
+                        {work.company} • {work.position}
+                        <MdArrowOutward id="work-link-arrow" className="inline w-5 ml-1 group-hover:animate-hoverDiagonal transition-transform"/>
+                    </h1>
+                    <hr className='mt-2 mx-auto opacity-50 sm:hidden' />  
+                    { work.description !== undefined ? (<p className="my-3 font-light lg:text-xl">{work.description}</p>) : null }
+                    { work.technologies !== undefined ?
+                        ( <div>
+                            { work.technologies.map((tech, techIndex) => (
+                                <span 
+                                    key={techIndex} 
+                                    className="inline-block px-3 py-1 mx-1 my-1 rounded-3xl bg-yellow-300 bg-opacity-80
+                                    dark:bg-yellow-400 dark:bg-opacity-50 text-sm font-semibold text-black dark:text-white"
+                                >
+                                    {tech}
+                                </span>
+                            
                             ))}
                         </div> ) :
                         null
