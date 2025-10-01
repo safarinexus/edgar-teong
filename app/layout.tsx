f921225cb8e940a469751ccd13699dac36335418
Edgar Teong <edgargarteong@gmail.com>
2025-10-01 19:30:17 +0800

    fixed missing metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,29 @@
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import { Providers } from "./providers";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter_Tight } from 'next/font/google';
+import type { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Edgar Teong | Software Engineer',
+  description: "Edgar's personal website", 
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/favicon-black.webp',
+        href: '/favicon-black.webp',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/favicon-white.webp',
+        href: '/favicon-white.webp',
+      },
+    ],
+  },
+}
 
 const inter = Inter_Tight({
   subsets: ['latin'],
