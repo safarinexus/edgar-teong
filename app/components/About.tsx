bb9230b0ec2a48a3ac7efc407d00c55e077818cc
Edgar Teong <edgar.teong@foodpanda.com>
2025-11-26 15:05:55 +0800

    updated company

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -246,7 +246,7 @@ const About = () => {
             </div>
             <div id="about-text-container" className="w-full pb-40 px-5 sm:px-24 font-light text-base">
                 <div id="about-body" className="mt-7 tracking-tight leading-relaxed font-light text-base lg:text-xl">
-                    <div id="about-para-1" className="mt-7 tracking-tight leading-relaxed font-light text-base lg:text-xl">Having graduated from Singapore Management Unversity, I&#39;m currently a Backend Engineer at <a href="https://www.foodpanda.com/about-foodpanda/"><span className="font-bold">foodpanda</span></a>. 
+                    <div id="about-para-1" className="mt-7 tracking-tight leading-relaxed font-light text-base lg:text-xl">Having graduated from Singapore Management Unversity, I&#39;m currently a Backend Engineer at <a href="https://www.foodpanda.com/about-foodpanda/"><span className="font-bold">foodpanda</span></a>&#92;<a href="https://www.deliveryhero.com"><span className="font-bold">Delivery Hero</span></a>. 
                     I am deeply passionate and fiercely self-driven. I love all things technology and building solutions for problems. It is one of my goals in life to be able to use that love to make a real difference to society through software.</div>
 
                     {/*
